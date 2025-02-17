6ac637ef775e8645a631c883e535cb27c11f33c1
c6 <31777460+c6-dev@users.noreply.github.com>
2025-02-17 11:23:11 +0400

    add yaml to gitignore, fix basepath not matching url regex

--- code/Util.cpp
+++ code/Util.cpp
@@ -14,6 +14,7 @@ namespace util {
         std::smatch match;
 
         URL result;
+        result.path = url;
         if (std::regex_match(url, match, r_url)) {
             result.protocol = match[1].str();
             result.host = match[2].str();   
